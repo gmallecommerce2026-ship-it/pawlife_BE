8aaab8a1da04459b54fc044a8cf3665091393cfa
Nguyen Thien An <an.nguyenthien112802@gmail.com>
2026-04-28 06:15:44 +0700

    updat

--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -1,12 +1,32 @@
 // src/modules/tags/tags.controller.ts
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
 import { TagsService } from './tags.service';
 import { CreateTagReportDto } from './dto/create-tag-report.dto';
+import { PrismaService } from 'src/database/prisma/prisma.service';
 
 @Controller('tags')
 export class TagsController {
-  constructor(private readonly tagsService: TagsService) {}
-
+  constructor(private readonly tagsService: TagsService, private prisma: PrismaService) {}
+  @Get('reports/:id')
+  async getTagReportDetail(@Param('id') id: string) {
+    const report = await this.prisma.tagReport.findUnique({
+      where: { id },
+      include: {
+        tag: {
+          include: {
+            pet: {
+              include: {
+                owner: true,
+                images: true,
+              }
+            }
+          }
+        }
+      }
+    });
+    if (!report) throw new NotFoundException('Report not found');
+    return report;
+  }
   @Get(':tagId/scan')
   async scanTag(@Param('tagId') tagId: string) {
     return this.tagsService.scanTag(tagId);
